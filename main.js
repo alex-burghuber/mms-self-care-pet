205336529a943807ce62f6bd76e947ee3cca2476
dani-aparicio <104635136+dani-aparicio@users.noreply.github.com>
2022-05-21 18:16:48 +0200

    implements powerBar

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-// const FOOD_DECAY_PER_HOUR = FOOD_DEFAULT_VALUE / 24; // Assumes food is empty after 24 hours
-const FOOD_DECAY_PER_HOUR = 10000; // For debugging
+// const FOOD_DECAY_PER_HOUR = DEFAULT_VALUE / 24; // Assumes food is empty after 24 hours
+// const POWER_DECAY_PER_HOUR = DEFAULT_VALUE /24; // Assumes power is empty after 24 hours
+const DECAY_PER_HOUR = 10000; // For debugging // for food and power is the same
+
+// const ASCEND_PER_HOUR = DEFAULT_VALUE / 8; // Assumes power is full after 8 hours
+const ASCEND_PER_HOUR = 50000; // for debugging 
+
+var sleeping;
 
 main();
 
@@ -16,30 +22,78 @@ function main() {
 function refresh() {
     const hoursPassed = getHoursPassedSinceLastTimestamp();
 
-    console.log(`Refreshing... Hours passed since last time: ${hoursPassed}h`);
+   // console.log(`Refreshing... Hours passed since last time: ${hoursPassed}h`);
 
     updateFood(hoursPassed);
+    updatePower(hoursPassed);
     saveLastTimeStamp();
 
     refreshUi();
 }
 
 function refreshUi() {
+    
     const food = getFood();
+    const power = getPower();
 
-    if (food <= FOOD_MIN_VALUE) {
+
+    if (food <= FOOD_MIN_VALUE) { //hungry
         document.getElementById("rabbit").src = "images/rabbit_hungry.gif";
-    } else {
+    }  else {
         document.getElementById("rabbit").src = "images/rabbit_idle.gif";
     }
 
+    if (sleeping){
+        document.getElementById("rabbit").src = "images/rabbit_sleep.gif";
+       if (power == DEFAULT_MAX_VALUE) {
+            document.getElementById("rabbit").src = "images/rabbit_jump.gif";
+            wakeUp();
+        } else if (power > POWER_MIDI_VALUE ) { //it is fit to wake up
+            document.getElementById("sleep-or-wake-up").onclick = onSleepOrWakeUpClicked;
+            document.getElementById("sleep-or-wake-up").innerHTML = "⏰🌄<br>WAKE UP";
+            document.getElementById("sleep-or-wake-up").style.boxShadow = ""; 
+
+        } else { // too tiered to wake up
+            document.getElementById("sleep-or-wake-up").onclick = "";
+            document.getElementById("sleep-or-wake-up").innerHTML = "";  
+            document.getElementById("sleep-or-wake-up").style.boxShadow = "none"; 
+        }
+    } else if (power > POWER_MIDI_VALUE) { // too activ, can´t sleep
+        document.getElementById("rabbit").src = "images/rabbit_vibing.gif";
+        document.getElementById("sleep-or-wake-up").onclick = "";
+        document.getElementById("sleep-or-wake-up").innerHTML = ""; 
+        document.getElementById("sleep-or-wake-up").style.boxShadow = "none";  
+
+    } else if (power > POWER_MIN_VALUE) { // it can try to sleep
+        document.getElementById("sleep-or-wake-up").onclick = onSleepOrWakeUpClicked; 
+        document.getElementById("sleep-or-wake-up").innerHTML = "💤💤<br>SLEEP";
+        document.getElementById("sleep-or-wake-up").style.boxShadow = "";  
+    } else { // tiered, it falls asleep
+        document.getElementById("rabbit").src = "images/rabbit_sleep.gif";
+        sleep();
+    }
+    
+    
+
     document.getElementById("foodBar").value = food;
+    document.getElementById("powerBar").value = power;
 }
 
 function updateFood(hoursPassed) {
-    const newFood = linearDecay(getFood(), FOOD_DECAY_PER_HOUR, hoursPassed);
+    const newFood = linearDecay(getFood(), DECAY_PER_HOUR, hoursPassed);
     saveFood(newFood);
 }
+
+function updatePower(hoursPassed) {
+    if(sleeping){  
+        const newPower = linearAscend(getPower(), ASCEND_PER_HOUR, hoursPassed);
+        savePower(newPower);
+    } else {
+        const newPower = linearDecay(getPower(), DECAY_PER_HOUR, hoursPassed);
+        savePower(newPower); 
+    }
+}
+
 
 function getHoursPassedSinceLastTimestamp() {
     return (currentTimestampInSeconds() - localStorage.getItem("last_timestamp")) / 3600
@@ -60,11 +114,43 @@ function linearDecay(currentValue, decayPerHour, hour) {
     return currentValue - decayPerHour * hour;
 }
 
+/**
+ * Linear ascend formula N(t) = N0 + a * t
+ * @param {number} currentValue N0
+ * @param {number} ascendPerHour a
+ * @param {number} hour t
+ * @returns Current value
+ */
+ function linearAscend(currentValue, ascendPerHour, hour) {
+    return parseFloat(currentValue) + parseFloat(ascendPerHour * hour);
+}
+
 function currentTimestampInSeconds() {
     return (new Date()).getTime() / 1000;
 }
 
 function onFeedClicked() {
-    saveFood(FOOD_DEFAULT_VALUE);
+    saveFood(DEFAULT_MAX_VALUE);
+    refresh();
+}
+
+function onSleepOrWakeUpClicked() {
+   if (sleeping) {
+        wakeUp();
+    } else {
+        sleep();
+    }
+
+}
+
+function sleep() {
+    sleeping = true;
+    document.getElementById("sleep-or-wake-up").classList.replace("sleep", "wake-up");  
+    refresh();
+}
+
+function wakeUp() {
+    sleeping = false;
+    document.getElementById("sleep-or-wake-up").classList.replace("wake-up", "sleep");
     refresh();
 }
--- storage.js
+++ storage.js
@@ -1,11 +1,15 @@
-const FOOD_DEFAULT_VALUE = 100;
+const DEFAULT_MAX_VALUE = 100; // default for all bars at max
 const FOOD_MIN_VALUE = 50;
 
+const POWER_MIN_VALUE = 10;
+const POWER_MIDI_VALUE = 60; 
+
+// food functions
 function getFood() {
     let food = localStorage.getItem("food");
     if (food === null) {
         // Setup food for the first time
-        saveFood(FOOD_DEFAULT_VALUE);
+        saveFood(DEFAULT_MAX_VALUE);
         food = localStorage.getItem("food");
     }
     return food;
@@ -14,8 +18,28 @@ function getFood() {
 function saveFood(value) {
     if (value < 0) {
         value = 0;
-    } else if (value > FOOD_DEFAULT_VALUE) {
-        value = FOOD_DEFAULT_VALUE;
+    } else if (value > DEFAULT_MAX_VALUE) {
+        value = DEFAULT_MAX_VALUE;
     }
     localStorage.setItem("food", value);
+}
+
+//power functions
+function getPower() {
+    let power = localStorage.getItem("power");
+    if (power === null) {
+        // Setup power for the first time
+        savePower(DEFAULT_MAX_VALUE);
+        power = localStorage.getItem("power");
+    }
+    return power;
+}
+
+function savePower(value) {
+    if (value < 0) {
+        value = 0;
+    } else if (value > DEFAULT_MAX_VALUE) {
+        value = DEFAULT_MAX_VALUE;
+    }
+    localStorage.setItem("power", value);
 }
